642af4e6140ad8e16bf0df8605849ac8553d7085
Egor Gavrilenko <bambloo769@gmail.com>
2023-06-13 11:28:11 +0300

    feat: implement read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,28 @@
+import { access, open } from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        await access(filePath);
+    } catch (e) {
+        throw new Error("File don't exist");
+    }
+
+    try {
+        const fd = await open(filePath);
+        const stream = fd.createReadStream();
+
+        stream.pipe(process.stdout)
+    } catch (e) {
+        console.log(e);
+    }
+
+
 };
 
 await read();
