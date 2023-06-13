ed3c46bedc3a178c1a8bb5b20a783ded3832cadc
Egor Gavrilenko <bambloo769@gmail.com>
2023-06-13 11:27:32 +0300

    feat: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,33 @@
+import { access, rename as renameP } from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const rename = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const oldPath = path.join(__dirname, "files", "wrongFilename.txt");
+    const newPath = path.join(__dirname, "files", "properFilename.md");
+
+    try {
+        await access(oldPath);
+    } catch (error) {
+        throw new Error("FS operation failed");
+    }
+
+    try {
+        await access(newPath);
+        throw new Error("FS operation failed");
+    } catch (e) {
+        if (e?.code !== "ENOENT") {
+            throw e;
+        }
+    }
+
+    try {
+        await renameP(oldPath, newPath);
+    } catch (e) {
+        throw e;
+    }
 };
 
 await rename();
