6dc7e714fccc428303b357f5014721f8e2fa1830
Egor Gavrilenko <bambloo769@gmail.com>
2023-06-13 11:26:59 +0300

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import { open } from 'node:fs';
+import { writeFile } from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const create = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+    try {
+
+        open(filePath, 'wx', (err, fd) => {
+            if (err?.code === "EEXIST") {
+                throw new Error("FS operation failed");
+            }
+        });
+
+        await writeFile(filePath, "I am fresh and young");
+    } catch (error) {
+        throw error;
+    }
 };
 
 await create();
