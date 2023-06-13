0993b87535f64e7d185e17a52c2f4306190dfbee
Egor Gavrilenko <bambloo769@gmail.com>
2023-06-13 11:28:23 +0300

    feat: implement write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,26 @@
+import { createWriteStream } from "node:fs"
+import { access } from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+    try {
+        await access(filePath);
+    } catch (e) {
+        throw new Error("File don't exist");
+    }
+
+    try {
+        const stream = createWriteStream(filePath);
+
+        process.stdin.pipe(stream);
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 await write();
