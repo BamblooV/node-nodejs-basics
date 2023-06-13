355e2e80a9b45b6bc340fcabefc66a47846f3454
Egor Gavrilenko <bambloo769@gmail.com>
2023-06-13 11:28:59 +0300

    feat: implement main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,46 @@
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { cpus } from "node:os"
+
+import { Worker } from "node:worker_threads"
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const workkerFile = path.join(__dirname, "worker.js");
+
+const workersLimit = cpus().length;
+
+const createWorker = (data) => {
+    return new Promise((res, rej) => {
+        const worker = new Worker(workkerFile, {
+            workerData: data,
+        });
+
+        worker.on('message', (msg) => res({ status: 'resolved', data: msg }));
+
+        worker.on('error', (e) => {
+            rej({ status: "error", data: null })
+        });
+
+        worker.on('exit', (code) => {
+            if (code !== 0)
+                rej(new Error(`Worker stopped with exit code ${code}`));
+        });
+    })
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const workers = [];
+
+    const offset = 2;
+    for (let i = offset; i < workersLimit + offset; i++) {
+        workers.push(createWorker(i));
+    }
+
+    const workersOut = await Promise.allSettled(workers)
+    console.log(workersOut);
 };
 
 await performCalculations();
