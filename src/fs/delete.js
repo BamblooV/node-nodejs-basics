9c3fff6e238eca51ff043f5aaff110b533a8f71d
Egor Gavrilenko <bambloo769@gmail.com>
2023-06-13 11:27:07 +0300

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import { access, rm } from 'node:fs/promises';
+import path, { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 const remove = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const pathToDelete = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await access(pathToDelete);
+    } catch (error) {
+        throw new Error("FS operation failed");
+    }
+
+    try {
+        await rm(pathToDelete);
+    } catch (e) {
+        throw e;
+    }
 };
 
 await remove();
