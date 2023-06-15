475826a3e346cc229e74692ac1c22309e6c8c5f3
Egor Gavrilenko <bambloo769@gmail.com>
2023-06-15 23:08:08 +0300

    reafactor: change output format

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,7 +2,7 @@ const parseArgs = () => {
     const parsedArgs = [];
     for (let i = 0; i < process.argv.length; i++) {
         if (process.argv[i].startsWith("--")) {
-            const pair = `${process.argv[i].slice(2)}=${process.argv[i + 1]}`
+            const pair = `${process.argv[i].slice(2)} is ${process.argv[i + 1]}`
             parsedArgs.push(pair);
             i++;
         }
